d6771941fa4378488d98ec7ed7e7cd7813109cd5
Inacio <joseinacio0398@gmail.com>
2024-05-23 21:39:02 -0300

    Teste de validação de e-mails

---
+++ TesteDeValidacaoDeEmail/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(" ** Digite um email para verificação: ** \n");
+        string email = Console.ReadLine();
+
+        string emailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$";
+        Regex regex = new Regex(emailPattern);
+
+        if (regex.IsMatch(email))
+        {
+            Console.WriteLine($"\n{email} é um email válido.");
+        }
+        else
+        {
+            Console.WriteLine($"\n{email} é um email inválido.");
+        }
+    }
+}
